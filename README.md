0e492679c10a6771adf822a04a6cca80524a83f6
dexdigi <dexterthomas@gmail.com>
2016-06-06 09:04:33 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-### A chrome extension that replaces following words in a web-page after it has been loaded
-- 'Refugee' to 'Human'
-- 'Refugees' to 'Humans'
+### A chrome extension that replaces 'transcended race' with 'was retroactively deemed safe by white people'
+because idk, why not
+
+more here: https://medium.com/@dexdigi/47649e79b3bd
+based on: https://github.com/todipratik/Replace-Words
--- content.js
+++ content.js
@@ -6,10 +6,20 @@ for (var i = 0; i < elements.length; i++) {
     for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
 
+//i'm sure there's a more elegant way to do all of this stuff but whatever, this works idc
+
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/Refugee/g, "Human");
-            replacedText = replacedText.replace(/refugee/g, "human");
+            var replacedText = text.replace(/Transcended Race/g, "Was Retroactively Deemed Safe by White People");
+            replacedText = replacedText.replace(/Transcended race/g, "was retroactively deemed safe by white people");
+            replacedText = replacedText.replace(/transcended race/g, "was retroactively deemed safe by white people");
+            replacedText = replacedText.replace(/Transcends Race/g, "Was Retroactively Deemed Safe by White People");
+            replacedText = replacedText.replace(/transcends race/g, "was retroactively deemed safe by white people");
+            replacedText = replacedText.replace(/transcending race/g, "being retroactively deemed safe by white people");
+            replacedText = replacedText.replace(/transcending racial/g, "being retroactively deemed safe by white people");
+            replacedText = replacedText.replace(/Transcend Race/g, "be retroactively deemed safe by white people");
+            replacedText = replacedText.replace(/transcend race/g, "be retroactively deemed safe by white people");
+
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
